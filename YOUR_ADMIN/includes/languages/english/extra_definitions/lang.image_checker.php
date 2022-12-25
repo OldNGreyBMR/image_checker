e8ce6444a9255bb791af62336ba87d91778ed257
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2022-12-25 16:59:31 +1000

    Add files via upload
    Image Checker for Zen Cart 1.5.8 and PHP 8.0 and 8.1

---
+++ YOUR_ADMIN/includes/languages/english/extra_definitions/lang.image_checker.php
@@ -0,0 +1,10 @@
+<?php
+// Plugin: Image Checker
+$define = [
+    'BOX_TOOLS_IMAGE_CHECKER' => 'Image Checker',
+];
+
+return $define;
+/*
+define('BOX_TOOLS_IMAGE_CHECKER', 'Image Checker');
+*/
---
+++ YOUR_ADMIN/includes/languages/english/lang.image_checker.php
@@ -0,0 +1,82 @@
+<?php
+// Plugin: Image Checker
+// English
+$define = [
+    'HEADING_TITLE' => 'Images Checker',
+    'TEXT_VERSION' => 'version ',
+    'TEXT_IMAGES_DIRECTORY' =>'Images path: '. 'DIR_FS_CATALOG_IMAGES',
+    
+    'TEXT_LIST_TYPE' => 'Check:',
+    'TEXT_CHECK_CATEGORIES' => 'Categories',
+    'TEXT_CHECK_PRODUCTS' => 'Products',
+    'TEXT_FILTERS' => 'Filters:',
+    'TEXT_LIST_ALL' => 'List all:',
+    'TEXT_LIST_DISABLED' => 'List disabled:',
+    'TEXT_LIST_NO_IMAGES' => 'List entries with no images defined:',
+    'TEXT_ENTRIES_CHECKED' => 'Entries checked: ',
+    'TEXT_IMAGE_PROBLEMS' => 'Image problems: ',
+    'TEXT_QUERY_LIMITED' => 'Results limited to <b>%s</b> entries.</b>',
+    
+    'TEXT_NO_ERRORS_FOUND' => '<p>There were no image errors found.<br />All entries with images defined refer to a valid image file, with the correct extension and of a common web image format.</p>',
+    
+    'ERROR_NO_ERROR' => 'ok',
+    'ERROR_NO_IMAGE_DEFINED' => 'No image defined',
+    'ERROR_IMAGE_NOT_FOUND' => 'Image not found',
+    'ERROR_NOT_IMAGE' => 'File is named as a <b>%s</b> but is not a valid image (as per getimagesize)',
+    'ERROR_IMAGE_FORMAT' => 'Image is named with a <b>%s</b> extension but is actually a <b>%s</b>!',
+    'ERROR_NOT_COMMON_FORMAT' => 'A <b>%s</b> file is not a type commonly used for web images (change to PNG/GIF/JPG or even BMP).',
+    
+    'TABLE_HEADING_ENTRY' => 'Entry',
+    'TABLE_HEADING_ID' => 'ID',
+    'TABLE_HEADING_MODEL' => 'Model',
+    'TABLE_HEADING_STATUS' => 'Status',
+    'TABLE_HEADING_NAME' => 'Name',
+    'TABLE_HEADING_IMAGE' => 'Image',
+    'TABLE_HEADING_RESULT' => 'Result',
+    
+    'TEXT_EDIT_CATEGORY' => 'Edit Category',
+    'TEXT_EDIT_PRODUCT' => 'Edit Product',
+
+];
+
+return $define;
+
+
+/*
+
+define('HEADING_TITLE', 'Images Checker');
+define('TEXT_VERSION', 'version ');
+define('TEXT_IMAGES_DIRECTORY','Images path: '. DIR_FS_CATALOG_IMAGES);
+
+define('TEXT_LIST_TYPE', 'Check:');
+define('TEXT_CHECK_CATEGORIES', 'Categories');
+define('TEXT_CHECK_PRODUCTS', 'Products');
+define('TEXT_FILTERS', 'Filters:');
+define('TEXT_LIST_ALL', 'List all:');
+define('TEXT_LIST_DISABLED', 'List disabled:');
+define('TEXT_LIST_NO_IMAGES', 'List entries with no images defined:');
+define('TEXT_ENTRIES_CHECKED', 'Entries checked: ');
+define('TEXT_IMAGE_PROBLEMS', 'Image problems: ');
+define('TEXT_QUERY_LIMITED', 'Results limited to <b>%s</b> entries.</b>');
+
+define('TEXT_NO_ERRORS_FOUND', '<p>There were no image errors found.<br />All entries with images defined refer to a valid image file, with the correct extension and of a common web image format.</p>');
+
+define('ERROR_NO_ERROR', 'ok');
+define('ERROR_NO_IMAGE_DEFINED', 'No image defined');
+define('ERROR_IMAGE_NOT_FOUND', 'Image not found');
+define('ERROR_NOT_IMAGE', 'File is named as a <b>%s</b> but is not a valid image (as per getimagesize)');
+define('ERROR_IMAGE_FORMAT', 'Image is named with a <b>%s</b> extension but is actually a <b>%s</b>!');
+define('ERROR_NOT_COMMON_FORMAT', 'A <b>%s</b> file is not a type commonly used for web images (change to PNG/GIF/JPG or even BMP).');
+
+define('TABLE_HEADING_ENTRY', 'Entry');
+define('TABLE_HEADING_ID', 'ID');
+define('TABLE_HEADING_MODEL', 'Model');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_NAME', 'Name');
+define('TABLE_HEADING_IMAGE', 'Image');
+define('TABLE_HEADING_RESULT', 'Result');
+
+define('TEXT_EDIT_CATEGORY', 'Edit Category');
+define('TEXT_EDIT_PRODUCT', 'Edit Product');
+
+*/
